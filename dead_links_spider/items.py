7bacc870dd18cc98173f6049c6db2daeb208ca9c
Roman Dryndik <dryndikroman@gmail.com>
2017-02-05 18:08:21 +0100

    Refactor

--- dead_links_spider/items.py
+++ dead_links_spider/items.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.item import Item, Field
 
 
-class DeadLinksSpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ResulItem(scrapy.Item):
+    url = Field()
+    referer = Field()
+    status = Field()
+
+    def __init__(self, url, referer, status, *args, **kwargs):
+        super(ResulItem, self).__init__(*args, **kwargs)
+        self['url'] = url
+        self['referer'] = referer
+        self['status'] = status
--- dead_links_spider/settings.py
+++ dead_links_spider/settings.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # Scrapy settings for dead_links_spider project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+ALLOWED_DOMANS = ["yplanapp.com"]
+START_URLS = ["https://yplanapp.com/bristol/nightlife"]
 
 BOT_NAME = 'dead_links_spider'
 
